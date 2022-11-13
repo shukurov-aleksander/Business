e3e339120bf032fd92ea06ea6e74c06993c9df41
Aleksander <Aleksander.Shukurov@gmail.com>
2022-11-14 00:09:01 +0300

    Changed exception massage with String.format(), reduce code repeat, refactoring

--- src/main/java/com/ku/business/dao/CompanyRepository.java
+++ src/main/java/com/ku/business/dao/CompanyRepository.java
@@ -23,18 +23,18 @@ public class CompanyRepository {
             LEFT JOIN storages s ON s.company_id=c.id
             LEFT JOIN details d ON c.id=d.company_id
         WHERE c.id = ?
-""";
+    """;
     public static final String FIND_ALL_QUERY = "SELECT * FROM companies";
     public static final String DELETE_BY_ID_QUERY = "DELETE FROM companies WHERE id = ?";
     public static final String INSERT_QUERY = """
         INSERT INTO companies (company_name, tax_number, user_id, is_government_agency) 
         VALUES (?, ?, ?, ?)
-""";
+    """;
     public static final String UPDATE_QUERY = """
         UPDATE companies 
         SET company_name = ?, tax_number =?, user_id = ?, is_government_agency = ? 
         WHERE id = ?
-""";
+    """;
     public static final String COMPANY_ID_COLUMN = "id";
     public static final String COMPANY_NAME_COLUMN = "company_name";
     public static final String COMPANY_TAX_NUMBER_COLUMN = "tax_number";
@@ -50,50 +50,45 @@ public class CompanyRepository {
     }
 
     public Company findById(Long id) {
-        Company company;
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_QUERY)
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_QUERY)
         ) {
-            statement.setLong(1, id);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                company = buildCompany(resultSet);
+            preparedStatement.setLong(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return buildCompany(resultSet);
             }
-            return company;
         } catch (Exception s) {
-            throw new RepositoryException("Can't find company with id=" + id, s);
+            throw new RepositoryException(String.format("Can't find company with id=%d!", id), s);
         }
     }
 
     private Company buildCompany(ResultSet resultSet) {
-        Company company;
-        List<Storage> storages = new ArrayList<>();
-        List<Detail> details = new ArrayList<>();
-        Storage storage;
-        Detail detail;
         try {
+            List<Storage> storages = new ArrayList<>();
+            List<Detail> details = new ArrayList<>();
             resultSet.next();
-            company = buildCompanyWithoutLists(resultSet);
+            Company company = buildCompanyWithoutLists(resultSet);
             do {
-                storage = buildStorage(resultSet);
-                if (!storages.contains(storage) && !storage.equals(new Storage())) {
+                Storage storage = buildStorage(resultSet);
+                if (!storages.contains(storage)) {
                     storages.add(storage);
                 }
-                detail = buildDetails(resultSet);
-                if (!details.contains(detail) && !detail.equals(new Detail()) ) {
+                Detail detail = buildDetails(resultSet);
+                if (!details.contains(detail)) {
                     details.add(detail);
                 }
             } while (resultSet.next());
-                company.setStorages(storages);
-                company.setDetails(details);
+            company.setStorages(storages);
+            company.setDetails(details);
+            return company;
         } catch (Exception s) {
             throw new RepositoryException("Result set is empty!", s);
         }
-        return company;
     }
 
     private Storage buildStorage(ResultSet resultSet) {
-        Storage storage = new Storage();
         try {
+            Storage storage = new Storage();
             if (resultSet.getString(STORAGE_ID_COLUMN) != null) {
                 storage.setId(resultSet.getLong(STORAGE_ID_COLUMN));
                 storage.setQuantity(resultSet.getInt(STORAGE_QUANTITY));
@@ -105,11 +100,11 @@ public class CompanyRepository {
     }
 
     private Detail buildDetails(ResultSet resultSet) {
-        Detail detail = new Detail();
         try {
+            Detail detail = new Detail();
             if (resultSet.getString(DETAIL_ID_COLUMN) != null) {
-            detail.setId(resultSet.getLong(DETAIL_ID_COLUMN));
-            detail.setOperationType(OperationType.valueOf(resultSet.getString(DETAIL_OPERATION_TYPE_COLUMN)));
+                detail.setId(resultSet.getLong(DETAIL_ID_COLUMN));
+                detail.setOperationType(OperationType.valueOf(resultSet.getString(DETAIL_OPERATION_TYPE_COLUMN)));
             }
             return detail;
         } catch (Exception s) {
@@ -133,57 +128,62 @@ public class CompanyRepository {
     }
 
     private Company buildCompanyWithoutLists(ResultSet resultSet) {
-        Company company = new Company();
         try {
+            Company company = new Company();
             company.setId(resultSet.getLong(COMPANY_ID_COLUMN));
             company.setCompanyName(resultSet.getString(COMPANY_NAME_COLUMN));
             company.setTaxNumber(resultSet.getString(COMPANY_TAX_NUMBER_COLUMN));
             company.setUserId(resultSet.getLong(COMPANY_USER_ID_COLUMN));
             company.setGovernmentAgency((resultSet.getBoolean(COMPANY_IS_GOVERNMENT_AGENCY_COLUMN)));
+            return company;
         } catch (Exception s) {
             throw new RepositoryException("Result set is empty!", s);
         }
-        return company;
     }
 
     public void save(Company company) {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(INSERT_QUERY)
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)
         ) {
-            statement.setString(1, company.getCompanyName());
-            statement.setString(2, company.getTaxNumber());
-            statement.setLong(3, company.getUserId());
-            statement.setBoolean(4, company.isGovernmentAgency());
-            statement.executeUpdate();
+            updateCompany(company, preparedStatement).executeUpdate();
         } catch (Exception e) {
-            throw new RepositoryException("Company with tax number=" + company.getTaxNumber() + " already exist", e);
+            throw new RepositoryException(String.format("Company with tax number=%s already exist", company.getTaxNumber()), e);
         }
     }
 
     public void update(Company company) {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY)
         ) {
-            statement.setString(1, company.getCompanyName());
-            statement.setString(2, company.getTaxNumber());
-            statement.setLong(3, company.getUserId());
-            statement.setBoolean(4, company.isGovernmentAgency());
-            statement.setLong(5, company.getId());
-            statement.executeUpdate();
+            updateCompany(company, preparedStatement);
+            preparedStatement.setLong(5, company.getId());
+            preparedStatement.executeUpdate();
         } catch (Exception e) {
-            throw new RepositoryException("Can't update company with id=" + company.getId() + ". This company is not exist!", e);
+            throw new RepositoryException(String.format("Can't update company with id=%d. This company is not exist!", company.getId()), e);
         }
+    }
+
+    public PreparedStatement updateCompany(Company company, PreparedStatement preparedStatement) {
+        try {
+            preparedStatement.setString(1, company.getCompanyName());
+            preparedStatement.setString(2, company.getTaxNumber());
+            preparedStatement.setLong(3, company.getUserId());
+            preparedStatement.setBoolean(4, company.isGovernmentAgency());
+        } catch (Exception e) {
+            throw new RepositoryException(String.format("Company with tax number=%s already exist", company.getTaxNumber()), e);
+        }
+        return preparedStatement;
     }
 
     public void delete(Long id) {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_QUERY)
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID_QUERY)
         ) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RepositoryException("Can't delete company with id=" + id + ". This company is not exist!", e);
+            throw new RepositoryException(String.format("Can't delete company with id=%d. This company is not exist!", id), e);
         }
     }
 }
