aac8b99af7bf6378494c930019b1dbd9dad33c3b
Aleksander Shukurov <86979190+shukurov-aleksander@users.noreply.github.com>
2022-11-19 22:29:50 +0300

    Feature/designing database (#11)
    * Add docker-compose file
    
    * Connect to liquibase, generate tables, generate test-data
    
    * Add "addForeignKeyConstraint"
    
    * Refactoring
    
    * separate tables to different *.xml files
    
    * Delete rollback, refactoring
    
    * Swap enum types

--- src/main/java/com/ku/business/BusinessApp.java
+++ src/main/java/com/ku/business/BusinessApp.java
@@ -1,8 +1,5 @@
 package com.ku.business;
 
-import com.ku.business.entity.Order;
-import com.ku.business.entity.OrderStatus;
-
 public class BusinessApp {
     public static void main(String[] args) {
     }
