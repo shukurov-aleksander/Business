bc49dda31c42583f3406b42e983510ae36342d59
Aleksander <Aleksander.Shukurov@gmail.com>
2023-02-11 19:14:12 +0300

    Remove DTO layer

--- src/main/java/com/ku/business/dto/ServiceDto.java
+++
@@ -1,8 +0,0 @@
-package com.ku.business.dto;
-
-public class ServiceDto {
-    private Long id;
-    private String serviceName;
-    private Long sum;
-    private String serviceDescription;
-}
--- src/main/java/com/ku/business/dto/StorageDto.java
+++
@@ -1,8 +0,0 @@
-package com.ku.business.dto;
-
-public class StorageDto {
-    private Long id;
-    private Integer quantity;
-    private CompanyDto company;
-    private ServiceDto service;
-}
