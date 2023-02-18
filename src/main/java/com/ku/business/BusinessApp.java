78e619220a6f2a13482e4072d0206506975fdb25
aleks <aleksandershukurov@gmail.com>
2023-02-18 23:36:30 +0300

    Initial commit, change javax to jakarta, update hibernate to 6.1.7 Fial version

--- src/main/java/com/ku/business/BusinessApp.java
+++ src/main/java/com/ku/business/BusinessApp.java
@@ -1,21 +1,21 @@
 package com.ku.business;
 
-import com.ku.business.config.JavaConfig;
-import com.ku.business.entity.Company;
 import com.ku.business.service.impl.CompanyServiceImpl;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
-public class BusinessApp {
+@SpringBootApplication
+public class BusinessApp implements CommandLineRunner {
+    @Autowired
+    CompanyServiceImpl service;
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
-        CompanyServiceImpl service = context.getBean(CompanyServiceImpl.class);
-        List<Company> companies = service.findAll();
-        for (Company company : companies) {
-            System.out.println(company);
-        }
+        SpringApplication.run(BusinessApp.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        service.findById(4L);
     }
 }
 
--- src/main/java/com/ku/business/entity/Order.java
+++ src/main/java/com/ku/business/entity/Order.java
@@ -1,16 +1,16 @@
 package com.ku.business.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 import java.util.List;
