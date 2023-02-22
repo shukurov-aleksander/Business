a4d5242e4304fa231eb805de72f33f3b24884897
Aleksander <Aleksander.Shukurov@gmail.com>
2023-02-23 00:17:31 +0300

    fix: main class

--- src/main/java/com/ku/business/BusinessApp.java
+++ src/main/java/com/ku/business/BusinessApp.java
@@ -1,9 +1,13 @@
 package com.ku.business;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class BusinessApp {
-
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApp.class, args);
+    }
 }
 
