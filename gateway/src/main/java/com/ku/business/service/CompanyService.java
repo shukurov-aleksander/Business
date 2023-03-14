cc56598786d954c6c308ef46407e61feaa9e9d62
Aleksander <Aleksander.Shukurov@gmail.com>
2023-03-14 22:48:01 +0300

    fix: refactoring

--- gateway/src/main/java/com/ku/business/service/CompanyService.java
+++ gateway/src/main/java/com/ku/business/service/CompanyService.java
@@ -9,28 +9,14 @@ public class CompanyService {
 
     public String findAll(CompanyFilter filter) {
         RestTemplate restTemplate = new RestTemplate();
-        StringBuilder fooResourceUrl = new StringBuilder("http://localhost:8080/companies?");
-        if (filter.getCompanyName() != null && !filter.getCompanyName().equals("")) {
-            fooResourceUrl.append("companyName=").append(filter.getCompanyName());
-        }
-        if (filter.getTaxNumber() != null && !filter.getTaxNumber().equals("")) {
-            fooResourceUrl.append("&taxNumber=").append(filter.getTaxNumber());
-        }
-        if (filter.getUserId() != null) {
-            fooResourceUrl.append("&userId=").append(filter.getUserId());
-        }
-        if (filter.getIsGovernmentAgency() != null) {
-            fooResourceUrl.append("&isGovernmentAgency=").append(filter.getIsGovernmentAgency());
-        }
-        if (filter.getCompanyStatus() != null) {
-            fooResourceUrl.append("&companyStatus=").append(filter.getCompanyStatus());
-        }
-        if (filter.getOffset() != null) {
-            fooResourceUrl.append("&offset=").append(filter.getOffset());
-        }
-        if (filter.getLimit() != null) {
-            fooResourceUrl.append("&limit=").append(filter.getLimit());
-        }
-        return restTemplate.getForObject(fooResourceUrl.toString(), String.class);
+        String fooResourceUrl = new StringBuilder("http://localhost:8080/companies?")
+            .append(filter.getCompanyName() != null ? new StringBuilder("&companyName=").append(filter.getCompanyName()) : "")
+            .append(filter.getTaxNumber() != null ? new StringBuilder("&taxNumber=").append(filter.getTaxNumber()) : "")
+            .append(filter.getUserId() != null ? new StringBuilder("&userId=").append(filter.getUserId()) : "")
+            .append(filter.getIsGovernmentAgency() != null ? new StringBuilder("&isGovernmentAgency=").append(filter.getIsGovernmentAgency()) : "")
+            .append(filter.getCompanyStatus() != null ? new StringBuilder("&companyStatus=").append(filter.getCompanyStatus()) : "")
+            .append(filter.getOffset() != null ? new StringBuilder("&offset=").append(filter.getOffset()) : "")
+            .append(filter.getLimit() != null ? new StringBuilder("&limit=").append(filter.getLimit()) : "").toString();
+        return restTemplate.getForObject(fooResourceUrl, String.class);
     }
 }
