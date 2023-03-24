2184598a9ef5eefe8daa29c25ec956d09bc7d2a5
shukurov-aleksander <aleksandershukurov@gmail.com>
2023-03-24 12:22:27 +0300

    fix: refactor, deleted useless classes

--- business/src/main/java/com/ku/business/repository/CompanyDao.java
+++ business/src/main/java/com/ku/business/repository/CompanyDao.java
@@ -1,7 +1,11 @@
 package com.ku.business.repository;
 
+import com.ku.business.dto.CompanyDto;
 import com.ku.business.dto.CompanyListDto;
+import com.ku.business.dto.DetailListDto;
+import com.ku.business.dto.StorageListDto;
 import com.ku.business.entity.CompanyStatus;
+import com.ku.business.entity.OperationType;
 import com.ku.business.filter.CompanyFilter;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +14,10 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Repository
 public class CompanyDao {
@@ -25,6 +32,15 @@ public class CompanyDao {
             AND (:isGovernmentAgency::text IS NULL OR c.is_government_agency = :isGovernmentAgency)
             AND (:companyStatus::company_status_enum IS NULL OR cs.company_status = :companyStatus::company_status_enum)
         LIMIT :limit OFFSET :offset
+    """;
+    private static final String FIND_BY_ID_QUERY = """
+        SELECT c.id, c.company_name, c.tax_number, c.is_government_agency, c.user_id, cs.company_status, s.id, s.quantity, d.id, d.operation_type
+        FROM companies c
+            LEFT JOIN company_status_histories csh on csh.company_id = c.id
+            LEFT JOIN company_statuses cs on csh.company_status_id = cs.id
+            LEFT JOIN storages s on s.company_id = c.id
+            LEFT JOIN details d on d.company_id = c.id
+        WHERE c.id = :id
     """;
 
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
@@ -54,6 +70,37 @@ public class CompanyDao {
                 .addValue("offset", filter.getOffset());
     }
 
+    public CompanyDto findById(Long id) {
+        return namedParameterJdbcTemplate.query(FIND_BY_ID_QUERY, filteredFieldsMap(id), this::buildCompanyDto);
+    }
+
+    @SneakyThrows
+    public CompanyDto buildCompanyDto(ResultSet rs) {
+        CompanyDto companyDto = new CompanyDto();
+        rs.next();
+        companyDto.setId(rs.getLong("id"))
+                .setCompanyName(rs.getString("company_name"))
+                .setTaxNumber(rs.getString("tax_number"))
+                .setIsGovernmentAgency(rs.getBoolean("is_government_agency"))
+                .setUserId(rs.getLong("user_id"))
+                .setCompanyStatus(CompanyStatus.valueOf(rs.getString("company_status")));
+        Set<DetailListDto> dtos = new HashSet<>();
+        Set<StorageListDto> storageListDtos = new HashSet<>();
+        do {
+            dtos.add(new DetailListDto()
+                    .setId(rs.getLong("id"))
+                    .setOperationType(OperationType.valueOf(Objects.toString(rs.getString("operation_type")))));
+            storageListDtos.add(new StorageListDto().setId(rs.getLong("id")).setQuantity(rs.getInt("quantity")));
+        }
+        while (rs.next());
+        companyDto.setDetails(dtos).setStorages(storageListDtos);
+
+        return companyDto;
+    }
+
+    public MapSqlParameterSource filteredFieldsMap(Long id) {
+        return new MapSqlParameterSource().addValue("id", id);
+    }
     @Autowired
     public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
--- business/src/main/java/com/ku/business/service/CompanyService.java
+++ business/src/main/java/com/ku/business/service/CompanyService.java
@@ -1,5 +1,6 @@
 package com.ku.business.service;
 
+import com.ku.business.dto.CompanyDto;
 import com.ku.business.dto.CompanyListDto;
 import com.ku.business.filter.CompanyFilter;
 import com.ku.business.repository.CompanyDao;
@@ -16,6 +17,9 @@ public class CompanyService {
         return companyDao.findAll(filter);
     }
 
+    public CompanyDto findById(Long id) {
+            return  companyDao.findById(id);
+    }
     @Autowired
     public void setCompanyDao(CompanyDao companyDao) {
         this.companyDao = companyDao;
